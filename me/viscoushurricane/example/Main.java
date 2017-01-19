351ec4406409524ae9e1219b7bd21707ed2884b7
ViscousHurricane <zachmelv@aol.com>
2017-01-19 08:00:29 -0500

    Update Main.java

--- me/viscoushurricane/example/Main.java
+++ me/viscoushurricane/example/Main.java
@@ -1,1 +1,14 @@
+package me.viscoushurricane.example
 
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Do Something
+    }
+    
+    @Override
+    public void onDisable() {
+        // Do Something
+    }
+}
